df7a42d5794a18160eef8646ed63ca956a264cf3
Kaunista <a9531712865@yandex.ru>
2017-03-03 16:55:27 +0300

    Add files via upload
    Master ver 03022017

---
+++ sql_server_connect/Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sql_server_connect
+{
+    class Class1
+    {/*
+        DECLARE @vendor_id int, @vendor_name nvarchar(50),  
+    @message varchar(80), @product nvarchar(50);  
+  
+PRINT '-------- Vendor Products Report --------';  
+  
+DECLARE vendor_cursor CURSOR FOR   
+SELECT VendorID, Name  
+FROM Purchasing.Vendor  
+WHERE PreferredVendorStatus = 1  
+ORDER BY VendorID;  
+  
+OPEN vendor_cursor  
+  
+FETCH NEXT FROM vendor_cursor   
+INTO @vendor_id, @vendor_name  
+  
+WHILE @@FETCH_STATUS = 0  
+BEGIN  
+    PRINT ' '  
+    SELECT @message = '----- Products From Vendor: ' +   
+        @vendor_name  
+  
+    PRINT @message  
+  
+    -- Declare an inner cursor based     
+    -- on vendor_id from the outer cursor.  
+  
+    DECLARE product_cursor CURSOR FOR   
+    SELECT v.Name  
+    FROM Purchasing.ProductVendor pv, Production.Product v  
+    WHERE pv.ProductID = v.ProductID AND  
+    pv.VendorID = @vendor_id  -- Variable value from the outer cursor  
+  
+    OPEN product_cursor  
+    FETCH NEXT FROM product_cursor INTO @product  
+  
+    IF @@FETCH_STATUS <> 0   
+        PRINT '         <<None>>'       
+  
+    WHILE @@FETCH_STATUS = 0  
+    BEGIN  
+  
+        SELECT @message = '         ' + @product  
+        PRINT @message  
+        FETCH NEXT FROM product_cursor INTO @product  
+        END  
+  
+    CLOSE product_cursor  
+    DEALLOCATE product_cursor  
+        -- Get the next vendor.  
+    FETCH NEXT FROM vendor_cursor   
+    INTO @vendor_id, @vendor_name  
+END   
+CLOSE vendor_cursor;  
+DEALLOCATE vendor_cursor;*/
+/////////////////////////////
+        /*SELECT name,id FROM sysobjects WHERE status>=0 AND type="U" ORDER BY name - tables 
+SELECT name,id FROM sysobjects WHERE status>=0 AND type="P" ORDER BY name - Stored Procedures 
+SELECT name,id FROM sysobjects WHERE status>=0 AND type="FN" ORDER BY name - User Defined Function*/
+    }
+}
---
+++ sql_server_connect/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.IO;
+using System.Data;
+
+namespace sql_server_connect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection sqlConnection = new SqlConnection();
+            //sqlConnection = new SqlConnection("Data Source=SQLEXPRESS; Integrated Security=SSPI; Initial Catalog=db3;");
+            //sqlConnection = new SqlConnection("Server=localhost\SQLEXPRESS; Database=db3; uid= sa; pwd=password1");
+            sqlConnection = new SqlConnection("Server=localhost" + "\\" + "SQLEXPRESS; Database=db3; uid= sa; pwd=password1");
+            //sqlConnection.Open();
+            Console.WriteLine("open connection");
+            HasRows5(sqlConnection);
+            sqlConnection.Close();
+            Console.WriteLine("close connection");
+            Console.ReadLine();
+            sqlConnection.Dispose();
+        }
+
+
+        static void HasRows5(SqlConnection connection)
+        {
+            using (connection)
+            {
+                SqlCommand command = new SqlCommand(
+                    // "select TABLE_NAME from INFORMATION_SCHEMA.TABLES where TABLE_TYPE = 'BASE TABLE' and TABLE_NAME != 'sysdiagrams'",
+                     "SELECT name FROM sysobjects WHERE status>=0 AND type='U' ORDER BY name;",
+                  connection);
+                connection.Open();
+
+
+                /*                                            */
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(@"C:\Users\Михаил\Documents\doc\write1.sql", true))
+                        {
+                            // file.WriteLine("Fourth line");
+                            file.WriteLine("{0}", reader.GetString(0));
+                            Console.WriteLine("{0}", reader.GetString(0));
+                            Console.WriteLine("записано в файл.");
+                            string name = reader.GetString(0);
+                            Console.WriteLine(name + " name");
+
+
+
+                            // "select TABLE_NAME from INFORMATION_SCHEMA.TABLES where TABLE_TYPE = 'BASE TABLE' and TABLE_NAME != 'sysdiagrams'",
+
+                        }
+                        Console.WriteLine("{0}", reader.GetString(0));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No rows found.");
+                }
+                reader.Close();
+            }
+            connection.Close();
+            SqlConnection connection1 = new SqlConnection();
+            connection1 = new SqlConnection("Server=localhost" + "\\" + "SQLEXPRESS; Database=db3; uid= sa; pwd=password1");
+            connection1.Open();
+            string s1;
+            /***************************************/
+            string path = @"C:\Users\Михаил\Documents\doc\write1.sql";
+
+
+
+            // This text is always added, making the file longer over time
+            // if it is not deleted.
+            /*Working*/
+            // Open the file to read from.co
+            string[] readText = File.ReadAllLines(path);
+            foreach (string s in readText)
+            {
+                string connectionString = "Server=localhost" + "\\" + "SQLEXPRESS; Database=db3; uid= sa; pwd=password1";
+                SqlDataReader reader;
+                Console.WriteLine("s= " + s);
+                string query = "Select * from " + s;
+                string connStr = @"Server=localhost" + "\\" + "SQLEXPRESS; Database=db3; uid= sa; pwd=password1";
+                string strDelimiter = ", ";
+                string strFilePath = @"C:\\Users\\Михаил\\Documents\\doc\\" + s + ".sql";
+                SqlCommand command1 = new SqlCommand(
+                    // "select TABLE_NAME from INFORMATION_SCHEMA.TABLES where TABLE_TYPE = 'BASE TABLE' and TABLE_NAME != 'sysdiagrams'",
+                     "select column_name from information_schema.columns where table_name ='" + s + "' order by ordinal_position", connection1);
+                //connection1.Open();
+                using (SqlConnection conn = new SqlConnection(connStr))
+                {
+                    conn.Open();
+                    using (reader = new SqlCommand(query, conn).ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            StringBuilder sb = new StringBuilder();
+                            Object[] items = new Object[reader.FieldCount];
+                            int counter = 0;
+                            while (reader.Read())
+                            {
+                                reader.GetValues(items);
+                                foreach (var item in items)
+                                {
+                                    // Using two sb.Append is better because if you 
+                                    // concat the two strings it will first build a new string
+                                    // and then discard it after its use.
+                                   
+                                    
+                                   // sb.Append("\n");
+
+                                    sb.Append(item.ToString());
+                                    sb.Append(strDelimiter);
+                                    counter += 1;
+                                    Console.WriteLine("counter " + counter);
+                                    if (counter % items.Length == 0)
+                                    {
+                                        sb.AppendLine();
+                                    }
+                                }
+                                sb.Append("\n");
+                                File.WriteAllText(strFilePath, sb.ToString());
+                            }
+                        }
+                    }
+                    conn.Close();
+                }
+
+
+                // Console.WriteLine(s);
+            }
+        }
+    }
+}
